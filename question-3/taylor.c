a322065e4240794287e2935aef72fa4bf0dbcc76
Gabriel Opiyo <160593612+GabrielNyawade@users.noreply.github.com>
2024-12-15 23:26:22 +0300

    Finish qn 3 and remove unnecessary Euler's number

--- question-3/taylor.c
+++ question-3/taylor.c
@@ -3,19 +3,21 @@
 
 int factorial(int n);
 int pwr(int base, int exponent);
-const float euler = 2.718281828;
 
 int main()
 {
     int x;
+    int terms;
     printf("Enter value of x for e^x: ");
     scanf("%d", &x);
+    printf("For how many terms?\n");
+    scanf("%d", &terms);
     double ans = 0;
-    for (int i = 0; i < x; i++)
+    for (int i = 0; i < terms; i++)
     {
-        ans += pwr(x, i) / factorial(i);
+        ans += (float) pwr(x, i) / factorial(i);
     }
-    printf("%.4f\n", ans);
+    printf("%f\n", ans);
     return 0;
 }
 
@@ -30,6 +32,15 @@ int pwr(int base, int exponent)
 {
     if (exponent == 0)
         return 1;
+    else if (exponent < 0)
+    {
+        exponent *= -1;
+        for (int i = 0; i < exponent; i++)
+        {
+            base *= base;
+        }
+        return 1 / base;
+    }
     else
     {
         for (int i = 0; i < exponent; i++)
