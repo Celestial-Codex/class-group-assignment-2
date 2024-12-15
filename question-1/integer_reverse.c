052760e77cdc0d587e4c29f7aaeb50cae8144d0b
Owen Ouna <pg.owenouna@gmail.com>
2024-12-16 00:00:18 +0300

    Update integer_reverse.c
    != operator will help with handling all integers( Both negative and positive)

--- question-1/integer_reverse.c
+++ question-1/integer_reverse.c
@@ -1,30 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  int number;
-  int remainder = 0;
-  int reversed_int = 0;
-  int isNegative = 0;
+    int main() {
+      int number;
+      int remainder = 0;
+      int reversed_int = 0;
 
-  printf("Enter number >>> ");
-  scanf("%d", &number);
+      printf("Enter number >>> ");
+      scanf("%d", &number);
 
-  if (number < 0) {
-    isNegative = 1;
-    number = -number;
-  }
+      while (number != 0) 
+      {
+        remainder = number % 10;
+        number -= remainder;
+        number /= 10;
+        reversed_int *= 10;
+        reversed_int += remainder;
+      }
 
-  while (number > 0) {
-    remainder = number % 10;
-    number -= remainder;
-    number /= 10;
-    reversed_int *= 10;
-    reversed_int += remainder;
-  }
-  if (isNegative) {
-    reversed_int *= -1;
-  }
-  printf("%d\n", reversed_int);
+      printf("%d\n", reversed_int);
 
-  return 0;
-}
+      return 0;
+    }
