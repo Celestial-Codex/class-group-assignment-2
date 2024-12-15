278de98e697640db327799a9f5992632ee0d8d46
benexl <benextempest@gmail.com>
2024-12-15 19:44:06 +0300

    feat: handle negative numbers

--- question-1/integer_reverse.c
+++ question-1/integer_reverse.c
@@ -4,9 +4,15 @@ int main() {
   int number;
   int remainder = 0;
   int reversed_int = 0;
+  int isNegative = 0;
 
   printf("Enter number >>> ");
   scanf("%d", &number);
+
+  if (number < 0) {
+    isNegative = 1;
+    number = -number;
+  }
 
   while (number > 0) {
     remainder = number % 10;
@@ -14,6 +20,9 @@ int main() {
     number /= 10;
     reversed_int *= 10;
     reversed_int += remainder;
+  }
+  if (isNegative) {
+    reversed_int *= -1;
   }
   printf("%d\n", reversed_int);
 
