04528021604c5cb574ded28d99ae167b6e4b3c8f
Gabriel Opiyo <>
2024-12-16 11:03:59 +0300

    add comments

--- question-3/taylor.c
+++ question-3/taylor.c
@@ -4,20 +4,20 @@
 #include <stdlib.h>
 #include <math.h>
 
-int factorial(int n);
+int factorial(int n);  //store the value of the factorial of n in n fact
 
 int main()
 {
     int x;
     int terms; // To add for a specific number of terms in the expansion
-    printf("Enter value of x for e^x: ");
+    printf("Enter value of x for e^x: ");  //allows user input to value of x
     scanf("%d", &x);
-    printf("For how many terms?\n");
+    printf("For how many terms?\n");  //allows user input to the number of of terms required by the user
     scanf("%d", &terms);
     double ans = 0;
-    for (int i = 0; i < terms; i++)
+    for (int i = 0; i < terms; i++)  //if i is equal to 0 and the number of terms are less than i then incriments by 1
     {
-        ans += pow(x, i) / factorial(i);
+        ans += pow(x, i) / factorial(i);  //increment the anwser with the value of the answer
     }
     printf("%f\n", ans);
     return 0;
@@ -25,9 +25,9 @@ int main()
 
 int factorial(int n)
 {
-    if (n == 0 || n == 1)
+    if (n == 0 || n == 1)  // returns the value of n when its greater than or equal to 0
         return 1;
     else
-        return n * factorial(n - 1);
+        return n * factorial(n - 1);  //returns the value of n
 }
 
